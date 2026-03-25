1e043c40c9d05896f29b748e6fb7f929a8ac0832
xut <1945707359@qq.com>
2026-03-25 23:50:07 +0800

    1.0.0

--- plugin.rb
+++ plugin.rb
@@ -10,22 +10,19 @@ require_relative "lib/community_hub"
 enabled_site_setting :community_hub_enabled
 
 after_initialize do
-  CommunityHub::Engine.routes.draw do
-    get "/hub-config.json" => "config#show"
+  # 公开 API
+  Discourse::Application.routes.append do
+    get "/hub-config.json" => "community_hub/config#show"
 
     # Admin UI API (仅管理员可访问)
-    get "/admin/plugins/community-hub/config.json" => "admin/config#index"
-    put "/admin/plugins/community-hub/nav_items.json" => "admin/config#save_nav_items"
-    put "/admin/plugins/community-hub/hero_banners.json" => "admin/config#save_hero_banners"
-    put "/admin/plugins/community-hub/sidebar_widgets.json" => "admin/config#save_sidebar_widgets"
+    get "/admin/plugins/community-hub/config.json" => "community_hub/admin/config#index"
+    put "/admin/plugins/community-hub/nav_items.json" => "community_hub/admin/config#save_nav_items"
+    put "/admin/plugins/community-hub/hero_banners.json" => "community_hub/admin/config#save_hero_banners"
+    put "/admin/plugins/community-hub/sidebar_widgets.json" => "community_hub/admin/config#save_sidebar_widgets"
 
-    delete "/admin/plugins/community-hub/nav_items/:id.json" => "admin/config#destroy_nav_item"
-    delete "/admin/plugins/community-hub/hero_banners/:id.json" => "admin/config#destroy_hero_banner"
-    delete "/admin/plugins/community-hub/sidebar_widgets/:id.json" => "admin/config#destroy_sidebar_widget"
-  end
-
-  Discourse::Application.routes.append do
-    mount ::CommunityHub::Engine, at: "/"
+    delete "/admin/plugins/community-hub/nav_items/:id.json" => "community_hub/admin/config#destroy_nav_item"
+    delete "/admin/plugins/community-hub/hero_banners/:id.json" => "community_hub/admin/config#destroy_hero_banner"
+    delete "/admin/plugins/community-hub/sidebar_widgets/:id.json" => "community_hub/admin/config#destroy_sidebar_widget"
   end
 end
 
